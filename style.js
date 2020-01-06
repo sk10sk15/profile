8c2a3f8a4003eda762d611a9a5203a03ec9f88b6
sk10sk15 <sk25sk25_1015@icloud.com>
2020-01-06 21:18:09 +0900

    add javascript

---
+++ style.js
@@ -0,0 +1,26 @@
+$(function(){
+  //HTML読み込み後
+  $(window).ready(function() {
+    $('.fadein-open').each(function(){
+      var targetElement = $(this).offset().top;
+      var load = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (load > targetElement - windowHeight + 200){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+      }
+    })
+  });
+  //スクロールしたら
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 250){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
+  });
+});
